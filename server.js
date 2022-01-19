2fe58f5fce9b65717ee252df6ed3cd14ddb654e2
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-01-19 13:41:45 -0700

    added db query

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,12 +7,29 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
 app.get('/', (req, res) => {
     res.json({
       message: 'Hello World'
     });
   });
-
+  
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
   // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
